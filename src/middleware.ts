0aa734830722f19330144630b1e5317ef0d56e23
Sadim Mali <sadimmalakar77@gmail.com>
2024-08-08 17:04:34 +0545

    refactor: Remove unwanted code

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,6 @@ export { default } from "next-auth/middleware";
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
-  console.log(request);
 
   //Redirect to dashboard if the user is already authenticated
   // and trying to access sign-in, sign-up, or home page
