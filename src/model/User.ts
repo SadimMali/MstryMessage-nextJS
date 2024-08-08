d22d4595d329fa5e8fdb7e3099ab1b085d54c106
Sadim Mali <sadimmalakar77@gmail.com>
2024-08-08 17:05:23 +0545

    refactor: Typography of isAcceptingMessages in UserSchema

--- src/model/User.ts
+++ src/model/User.ts
@@ -24,7 +24,7 @@ export interface User extends Document {
   verifyCode: string;
   verifyCodeExpiry: Date;
   isVerified: boolean;
-  isAccepting: boolean;
+  isAcceptingMessages: boolean;
   messages: Message[];
 }
 
@@ -57,7 +57,7 @@ const UserSchema: Schema<User> = new Schema({
     type: Boolean,
     default: false,
   },
-  isAccepting: {
+  isAcceptingMessages: {
     type: Boolean,
     default: true,
   },
