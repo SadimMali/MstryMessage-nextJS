f5b612b6098905912f1d722bb13a0140b3dc46ac
Sadim Mali <sadimmalakar77@gmail.com>
2024-08-10 10:38:24 +0545

    feat(verify/username): Implement api to verify code and create ui to retrieve code in client side

---
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -0,0 +1,99 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast";
+import { verifySchema } from "@/schemas/verifySchema";
+import { ApiResponse } from "@/types/ApiResponse";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import { Loader2 } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+const page = () => {
+  const router = useRouter();
+  const params = useParams<{ username: string }>();
+  const { toast } = useToast();
+  const form = useForm<z.infer<typeof verifySchema>>({
+    resolver: zodResolver(verifySchema),
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onSubmit = async (data: z.infer<typeof verifySchema>) => {
+    setIsSubmitting(true);
+    try {
+      const response = await axios.post("/api/verify-code", {
+        username: params.username,
+        code: data.code,
+      });
+      toast({
+        title: "Success",
+        description: response.data.message,
+      });
+      router.replace("/sign-in");
+      setIsSubmitting(false);
+    } catch (error) {
+      console.error("Error verifying code", error);
+      const axiosError = error as AxiosError<ApiResponse>;
+
+      //Default error message
+      let errorMessage = axiosError.response?.data.message;
+      toast({
+        title: "Error verifying code",
+        description: errorMessage,
+        variant: "destructive",
+      });
+      setIsSubmitting(false);
+    }
+  };
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Verify Your Account
+          </h1>
+          <p className="mb-6">Enter the verification code sent to your email</p>
+        </div>
+        {/* form */}
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="code"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Verification code</FormLabel>
+                  <FormControl>
+                    <Input placeholder="code" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">
+              {isSubmitting ? (
+                <>
+                  <Loader2 className="h-4 w-4 animate-spin mr-2" />  Please wait....
+                </>
+              ) : (
+                "Verify"
+              )}
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default page;
